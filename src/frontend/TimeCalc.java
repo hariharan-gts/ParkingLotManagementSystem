d16af79d3d85af0516ab96008762e5412a0afe4d
Kirubha12 <100814426+Kirubha12@users.noreply.github.com>
2022-10-13 08:47:10 +0530

    Update TimeCalc.java

--- src/frontend/TimeCalc.java
+++ src/frontend/TimeCalc.java
@@ -20,16 +20,18 @@ public class TimeCalc {
 		SimpleDateFormat sdf = new SimpleDateFormat("dd:MM:yyyy HH:mm:ss");
 		long ans = 0;
 		try {
+			// CALCULATING THE TOTAL HOURS THE VEHICLE WAS PARKED
+			
 			Date d1 = sdf.parse(IN);
             Date d2 = sdf.parse(Out);
             long difference_In_Time = d2.getTime() - d1.getTime();
-           // System.out.println(difference_In_Time);
             long difference_In_Hours = TimeUnit.MILLISECONDS.toHours(difference_In_Time) % 24;
             long difference_In_Days = TimeUnit.MILLISECONDS.toDays(difference_In_Time) % 365;
             ans = difference_In_Days*24 + difference_In_Hours;
 		}catch(ParseException e) {
 			e.printStackTrace();
 		}
+		//calling bill generation method in BillGeneration class
 		setfAns(new BillGeneration().BillCalculation(ans, type));
 		 return getfAns();
 		
